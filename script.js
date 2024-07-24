84cc5de8965b7a9e76600ffb579af74743010571
Adamam <88430865+Adamam13@users.noreply.github.com>
2024-07-24 17:09:36 +0700

    add popup

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var file1 = document.querySelector(".file1_popup");
 
-file1.addEventListener()
+var file1 = document.querySelector("#file_1");
+
+file1.addEventListener("click", popup);
+
+function popup(){
+    console.log('hello1');
+    var file_popup = document.querySelector("#file1_popup");
+    console.log('hello2');
+    file_popup.dataset.view = '1';
+    console.log('hello3');
+}
 
