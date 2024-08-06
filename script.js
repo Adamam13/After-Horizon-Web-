83a98e638f8eed37f2cb0bf50f74661d847cd90e
TonNam.exe <88495758+ParinTonNam@users.noreply.github.com>
2024-08-07 01:03:26 +0700

    test-file-1

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 
-var file1 = document.querySelector("#file_1");
+// var file1 = document.querySelector("#file_1");
 
-file1.addEventListener("click", popup);
+// file1.addEventListener("click", popup);
 
-function popup(){
-    console.log('hello1');
-    var file_popup = document.querySelector("#file1_popup");
-    console.log('hello2');
-    file_popup.dataset.view = '1';
-    console.log('hello3');
+// function popup(){
+//     console.log('hello1');
+//     var file_popup = document.querySelector("#file1_popup");
+//     console.log('hello2');
+//     file_popup.dataset.view = '1';
+//     console.log('hello3');
+// }
+
+function openWindow(id) {
+    document.getElementById(id).style.display = 'flex';
 }
 
+function closeWindow(id) {
+    document.getElementById(id).style.display = 'none';
+}
